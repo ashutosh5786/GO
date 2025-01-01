f6221b29be929477b67611a920aca956d03e7f78
ashutosh5786 <ashutosh5786@outlook.com>
2025-01-01 21:24:26 +0000

    Starting

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module hello
+module app
 
 go 1.23.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package app
+
+import ("fmt")
+
+func main() {
+	fmt.Println("Started to buuild the application which install application on linxu machines")
+}
