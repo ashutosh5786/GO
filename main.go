608605fa50f006438b913e5073badef467682116
ashutosh5786 <ashutosh5786@outlook.com>
2025-01-01 22:09:37 +0000

    cheking which packagemanger is present

--- main.go
+++ main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
+	"strings"
 )
 
+func findPackagemanger() string {
+	packageManagers := []string{"apt", "yum", "dnf", "zypper", "pacman", "emerge"}
+	for _, pm := range packageManagers {
+		if _, err := os.Stat("/usr/bin/" + pm); err == nil {
+			return pm
+		}
+	}
+	return ""
+}
+
 func main() {
-	fmt.Println("Started to buuild the application which install application on linxu machines")
+	if runtime.GOOS != "linux" && runtime.GOARCH != "amd64" {
+		fmt.Println("This program only runs on Linux 64-bit.")
+		os.Exit(1)
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <package1> <package2> ...")
+		os.Exit(1)
+	}
+
+	packages := os.Args[1:]
+	fmt.Println("Packages to install :", strings.Join(packages, ", "))
+
+
+	PackageManger := findPackagemanger() // Checking if package Manger Exist
+	if PackageManager == "" {
+		fmt.Println("No package manager found.")
+		os.Exit(1)
+	}
 }
